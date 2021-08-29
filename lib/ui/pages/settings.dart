5885644ea46a3c0f2f6f7a130e6a78fbad6946df
yerry <yerry@DESKTOP-QM6O484>
2021-08-28 18:47:04 -0600

    Designing Settings Page for privacy policy, delete profile, and edit profile

--- lib/ui/pages/settings.dart
+++ lib/ui/pages/settings.dart
@@ -1,20 +1,85 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test2/ui/constants.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        backgroundColor: backgroundColor,
+        centerTitle: true,
+        title: Text('Settings'
+        ),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-            Navigator.pop(context);
-          },
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              margin: const EdgeInsets.all(8.0),
+              child: ListTile(
+                onTap: (){
+                  //open edit profile
+                },
+                title: Text("Edit Profile") ,
+                trailing: Icon(Icons.edit,color: backgroundColor,) ,
+              ),
+            ),
+            const SizedBox(height: 10.0,),
+            Card(
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+           child: Column(
+             children: <Widget>[
+
+               ListTile(
+                 leading: Icon(Icons.policy_outlined, color: backgroundColor,),
+                 title: Text("Privacy Policy"),
+                 trailing: Icon(Icons.keyboard_arrow_right,color: backgroundColor,),
+                   onTap:(){
+                   //open privacy policy
+                   }
+
+               ),
+                Divider(
+                  color: Colors.black,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+               ListTile(
+                   leading: Icon(Icons.delete, color: backgroundColor,),
+                   title: Text("Delete Profile"),
+                   trailing: Icon(Icons.keyboard_arrow_right,color: backgroundColor,),
+                   onTap:(){
+                     //open privacy policy
+                   }
+
+               ),
+             ],
+
+            ),
+            ),
+            const SizedBox(height: 10.0),
+            Text("Notification Settings", style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.indigo,
+            ),
+            ),
+            SwitchListTile(value: true,
+            activeColor: Colors.indigo,
+            contentPadding: const EdgeInsets.all(0),
+            title: Text("Receive Notifications"),
+              onChanged: (val){
+              // flip
+              },
+            ),
+          ],
+
         ),
       ),
     );
