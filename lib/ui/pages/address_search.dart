724f6c79c3db8caf1e03493600347c38438372e4
jonengland <jonhengland@hotmail.com>
2021-09-20 14:36:17 -0600

    fixed the search button null case bug

--- lib/ui/pages/address_search.dart
+++ lib/ui/pages/address_search.dart
@@ -36,8 +36,29 @@ class AddressSearch extends SearchDelegate<Suggestion> {
 
   @override
   Widget buildResults(BuildContext context) {
-    print("build results");
-    return null;
+    return FutureBuilder(
+      future: query == ""
+          ? null
+          : apiClient.fetchSuggestions(
+          query, Localizations.localeOf(context).languageCode),
+      builder: (context, snapshot) => query == ''
+          ? Container(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Enter your address'),
+      )
+          : snapshot.hasData
+          ? ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          title:
+          Text((snapshot.data[index] as Suggestion).description),
+          onTap: () {
+            close(context, snapshot.data[index] as Suggestion);
+          },
+        ),
+        itemCount: snapshot.data.length,
+      )
+          : Container(child: Text('Loading...')),
+    );
   }
 
   @override
